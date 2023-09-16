f63f42586c36d98221aa7f2938e8596d89a0934f
Trey Torrez <treytorrez@gmail.com>
2023-09-16 14:47:32 -0600

    Finished Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+            Console.WriteLine("What is your grade?");
+            Console.Write("Grade: ");
+            int grade = Convert.ToInt16(Console.ReadLine());
+            switch (grade)
+            {
+                case int n when grade >= 90:
+                    Console.WriteLine("A");
+                    break;
+                case int n when grade >= 80:
+                    Console.WriteLine("B");
+                    break;
+                case int n when grade >= 70:
+                    Console.WriteLine("C");
+                    break;
+                case int n when grade >= 60:
+                    Console.WriteLine("F");
+                    break;
+                default:
+                    Console.WriteLine("Not a Valid Grade");
+                    break;
+            
+            }
     }
 }
