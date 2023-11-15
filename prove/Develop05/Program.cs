f072de7c80a53d0ebaf813ddfc4ad3803cadc171
Trey Torrez <treytorrez@gmail.com>
2023-11-14 19:22:41 -0700

    Testing How branches work

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -21,27 +21,7 @@ class Program
             switch (CurrentPage)
             {
                 case "main":
-                    // make important text bold; \x1b[1m{content}\x1b[22m
-                    Console.WriteLine("\x1b[1mWhat would you like to do?\x1b[22m");
-                    Console.WriteLine("  Create a new goal (\x1b[1mcreate\x1b[22m)");//create
-                    Console.WriteLine("  View existing goals (\x1b[1mview\x1b[22m)");//view
-                    Console.WriteLine("  Exit (\x1b[1mexit\x1b[22m)");//exit
-                    CurrentPage = Console.ReadLine().ToLower();
-                    break;
-                case "create":
-                    Console.WriteLine("What type of goal would you like to create?");
-                    Console.WriteLine("Infinite (inf) or Finite (fin)");
-                    Console.WriteLine("Return to main menu (main)");
-                    string goalType = Console.ReadLine().ToLower();
-                    //Add filler for now
-                    Console.WriteLine("Functionality not yet implemented.");
-                    Console.WriteLine("press enter to continue");
-                    Console.ReadLine();
-                    //TODO: Complete create goal functionality
-                    break;
-                case "view":
-                    //TODO: ViewGoals();
-                    // Same bold styling as above
+                    // make imp
                     Console.WriteLine("\x1b[1mWhat would you like to do?\x1b[22m");
                     Console.WriteLine();
                     Console.WriteLine("Mark a goal as complete (\x1b[1mmark\x1b[22m])");//mark
